5cd9947e690fa6342e6ab7adf269dfbd35c66bde
Babybooapps <Babybooapps@gmail.com>
2024-01-07 18:27:34 +0530

    no message

---
+++ Real Extreme Bike Stunts/Assets/RotateWheelScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWheelScript : MonoBehaviour {
+
+	public Vector3 rot;
+	void Start () {
+		
+	}
+	
+
+	void Update () 
+	{
+		this.transform.Rotate (rot.x, rot.y, rot.z);
+	}
+}
